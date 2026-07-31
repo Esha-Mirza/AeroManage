be415c510baf73071c2c2fc90f41e14609f5b917
Esha-Mirza <esha101374@gmail.com>
2026-08-01 02:03:10 +0500

    Added MealSelection header file

---
+++ include/MealSelection.h
@@ -0,0 +1,15 @@
+#include<iostream>
+#ifndef MEALSELECTION_H
+#define MEALSELECTION_H
+using namespace std;
+
+#include <string>
+
+class MealSelection {
+public:
+    string type;
+    void selectType();
+};
+
+
+#endif
